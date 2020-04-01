Fix evaluation and fix improper network rotation

loss.py
@@ -1,7 +1,11 @@
 
 # STD LIB
+import os
 import pdb
+import glob
 import logging
+import pathlib
+import argparse
 from collections import namedtuple
 
 # EXERNAL LIB
@@ -10,7 +14,10 @@ import torch.nn as nn
 from torchvision import models
 
 import cv2
+import flowiz
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # LOCAL LIB
 try:
@@ -55,6 +62,10 @@ class Vgg16(torch.nn.Module):
 class StyleTransferVideoLoss():
     def __init__(self, style_fname):
         style_img = cv2.imread(style_fname)
+        
+        style_basename = os.path.splitext(os.path.basename(style_fname))[0]
+        style_weights = {'WomanHat': 20, 'picasso': 10, 'candy': 10, 'mosaic': 10, 'scream': 20}
+        self.style_weight = style_weights[style_basename]
 
         self.vgg = Vgg16(requires_grad=False)
         style_features = self.vgg(styutils.preprocess(style_img))
@@ -63,35 +74,116 @@ class StyleTransferVideoLoss():
     
     def gram_matrix(self, tensor):
         n, c, h, w = tensor.size()
-        features = tensor.view(n, c, h*w)
-        features_t = features.transpose(1, 2)
-        gram = features.bmm(features_t) / (c*h*w)
-        return gram
+        flat = tensor.view(n*c, -1)
+        out = torch.mm(flat, flat.t()) / (c*h*w)
+        return out 
 
     def eval(self, img, out, multi=None):
+        h, w = img.shape[0], img.shape[1]
         img_features = self.vgg(styutils.preprocess(img))
         out_features = self.vgg(styutils.preprocess(out))
         
-        content_loss = self.mse_loss(img_features.relu2_2, out_features.relu2_2)
+        content_loss = self.mse_loss(img_features.relu3_3, out_features.relu3_3)
         
         style_loss = 0
         for feature, target_gram in zip(out_features, self.style_gram):
             out_gram = self.gram_matrix(feature)
             style_loss += self.mse_loss(out_gram, target_gram)
+        style_loss *= self.style_weight
         
         temporal_loss = 0
         if multi:
-            prev, flow, cert = multi
-            pre_cert = torch.FloatTensor(np.swapaxes(cert / 255, 0, 1)).unsqueeze(0).unsqueeze(0)
-            prev_warped = torch.FloatTensor(np.swapaxes(styutils.warp(prev, flow), 0, 2)).unsqueeze(0)
             # The masked portions do not influence the temporal loss.
             # FIXME: Is it a bug that the pre_cert does not use the min filter?
+            prev, flow, cert = multi
+            prev_warped = torch.FloatTensor(styutils.warp(prev, flow)).view(1, 3, h, w)
+            pre_cert = torch.FloatTensor(cert / 255).unsqueeze(0).unsqueeze(0)
             prev_warped_masked = prev_warped * pre_cert.expand_as(prev_warped)
-            out_masked = torch.FloatTensor(np.swapaxes(out, 0, 2)) * pre_cert.expand_as(prev_warped)
+            out_pre = torch.FloatTensor(out).view(1, 3, h, w)
+            out_masked = out_pre * pre_cert.expand_as(out_pre)
             temporal_loss = self.mse_loss(prev_warped_masked, out_masked)
         
         logging.debug('CONTENT:\t{}\tSTYLE:\t{}\tTEMPORAL:\t{}'.format(
             content_loss, style_loss, temporal_loss))
         score = content_loss + style_loss + temporal_loss
-        logging.info('Loss:\t{}'.format(score))
-        return 
+        logging.debug('Loss:\t{}'.format(score))
+        return content_loss, style_loss, temporal_loss
+
+def parse_args():
+    '''Parses arguments.'''
+    ap = argparse.ArgumentParser()
+    
+    # Required arguments
+    ap.add_argument('style', type=str,
+        help='The style image to evaluate against')
+    ap.add_argument('src_dir', type=str,
+        help='The path to the original .ppm, .flo, .pgm files')
+    ap.add_argument('fav_dir', type=str,
+        help='The path to the output of FAV by Ruder et al.')
+    ap.add_argument('dav_dir', type=str,
+        help='The path to the output of DAV')
+    
+    return ap.parse_args()
+
+def main():
+    args = parse_args()
+    styutils.start_logging()
+    
+    assert(os.path.exists(args.style))
+    l = StyleTransferVideoLoss(args.style)
+    
+    ppm_names = [str(pathlib.Path(args.src_dir) / name) for name in glob.glob1(args.src_dir, '*.ppm')]
+    flo_names = [str(pathlib.Path(args.src_dir) / name) for name in glob.glob1(args.src_dir, 'backward*.flo')]
+    pgm_names = [str(pathlib.Path(args.src_dir) / name) for name in glob.glob1(args.src_dir, '*.pgm')]
+    fav_names = [str(pathlib.Path(args.fav_dir) / name) for name in glob.glob1(args.fav_dir, '*.png')]
+    dav_names = [str(pathlib.Path(args.dav_dir) / name) for name in glob.glob1(args.dav_dir, '*.png')]
+    
+    flo_names = sorted(flo_names, key=lambda x: len(x))
+    pgm_names = sorted(pgm_names, key=lambda x: len(x))
+    
+    ppms = [cv2.imread(fname) for fname in ppm_names]
+    flos = [None] + [flowiz.read_flow(fname) for fname in flo_names]
+    pgms = [None] + [cv2.imread(fname, cv2.IMREAD_UNCHANGED) for fname in pgm_names]
+    favs = [cv2.imread(fname) for fname in fav_names]
+    davs = [cv2.imread(fname) for fname in dav_names]
+    
+    assert(len(ppms) > 0)
+    logging.info('Images loaded...')
+    
+    fc_ls, fs_ls, ft_ls, f_tls = [], [], [], []
+    dc_ls, ds_ls, dt_ls, d_tls = [], [], [], []
+    
+    for i in range(len(davs)):
+        if i == 0:
+            fc_l, fs_l, ft_l = map(int, l.eval(ppms[i], favs[i]))
+            dc_l, ds_l, dt_l = map(int, l.eval(ppms[i], davs[i]))
+            
+            fc_ls.append(fc_l)
+            fs_ls.append(fs_l)
+            f_tls.append(int(fc_l + fs_l))
+            dc_ls.append(dc_l)
+            ds_ls.append(ds_l)
+            d_tls.append(int(dc_l + ds_l))
+        else:
+            fc_l, fs_l, ft_l = map(int, l.eval(ppms[i], favs[i], (favs[i-1], flos[i], pgms[i])))
+            dc_l, ds_l, dt_l = map(int, l.eval(ppms[i], davs[i], (davs[i-1], flos[i], pgms[i])))
+            
+            fc_ls.append(fc_l)
+            fs_ls.append(fs_l)
+            ft_ls.append(ft_l)
+            f_tls.append(int(fc_l + fs_l + ft_l))
+            dc_ls.append(dc_l)
+            ds_ls.append(ds_l)
+            dt_ls.append(dt_l)
+            d_tls.append(int(dc_l + ds_l + dt_l))
+    
+    df = pd.DataFrame({ 
+        'fav_content': fc_ls, 'fav_style': fs_ls, 'fav_temporal': [0] + ft_ls, 
+        'dav_content': dc_ls, 'dav_style': ds_ls, 'dav_temporal': [0] + dt_ls, 
+        'fav_total': f_tls, 'dav_total': d_tls
+    }).astype(int)
+    logging.info(df)
+    df.to_csv('_' + os.path.basename(args.src_dir) + '_loss.csv')
+
+if __name__ == '__main__':
+    main()

model.py
@@ -70,7 +70,7 @@ class Interpolate(nn.Module):
             size=self.size, scale_factor=self.scale_factor, mode=self.mode, align_corners=self.align_corners)
 
 class StylizationModel():
-    def __init__(self, weights_fname=None, style_fname=None):
+    def __init__(self, weights_fname=None, seed_fname=None, style_fname=None):
     
         # Main stylization network
         self.model = nn.Sequential( # Sequential,
@@ -179,6 +179,7 @@ class StylizationModel():
         self.eval = False
         if weights_fname:
             self.set_fname(weights_fname)
+        self.seed_fname = seed_fname
         if style_fname:
             assert(os.path.exists(style_fname))
             self.style_fname = style_fname
@@ -202,10 +203,10 @@ class StylizationModel():
         
     def run_next_image(self, img, prev, flow, cert):
         start = time.time()
-        # Preprocess the current input image
+        # Preprocess the current input image (h, w, 3) -> (1, 3, h, w)
         pre = styutils.preprocess(img)
-        # Consistency check preprocessing: Apply min filter and swap axes
-        pre_cert = self.min_filter.forward(torch.FloatTensor(np.swapaxes(cert / 255, 0, 1)).unsqueeze(0).unsqueeze(0))
+        # Consistency check preprocessing: Apply min filter
+        pre_cert = self.min_filter.forward(torch.FloatTensor(cert / 255).unsqueeze(0).unsqueeze(0))
         # Warp the previous output with the optical flow between the new image and previous image
         prev_warped = styutils.warp(prev, flow)
         # Apply preprocessing to the warped image
@@ -243,8 +244,11 @@ class StylizationModel():
             img = cv2.imread(fname)
             
             if idx == 0:
-                # Independent style transfer is equivalent to Fast Neural Style by Johnson et al.
-                out = self.run_image(img)
+                if self.seed_fname:
+                    out = cv2.imread(self.seed_fname)
+                else:
+                    # Independent style transfer is equivalent to Fast Neural Style by Johnson et al.
+                    out = self.run_image(img)
                 if self.eval: crit.eval(img, out, None)
             else:
                 flowname = str(remote / 'backward_{}_{}.flo'.format(idx + start + 1, idx + start))
@@ -265,8 +269,8 @@ class StylizationModel():
                 out = self.run_next_image(img, out, flow, cert)
                 if self.eval: crit.eval(img, out, (pout, flow, cert))
                 # Remove unnecessary files to save space.
-                os.remove(flowname)
-                os.remove(certname)
+                #os.remove(flowname)
+                #os.remove(certname)
             
             logging.info('Writing to {}...'.format(out_fname))
             cv2.imwrite(out_fname, out)

styutils.py
@@ -32,7 +32,7 @@ def preprocess(img):
     # Swap RGB to BGR (this appears unnecessary as cv2 is already BGR)
     #bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     # Swap axes
-    tmp = np.swapaxes(img, 0, 2)
+    tmp = np.swapaxes(np.swapaxes(img, 0, 1), 0, 2)
     
     # Unsqueeze
     usq = torch.Tensor(tmp).unsqueeze(0)
@@ -56,7 +56,7 @@ def deprocess(img):
     sqz = torch.squeeze(add).detach().numpy()
     
     # Swap axes
-    tmp = np.swapaxes(sqz, 0, 2)
+    tmp = np.swapaxes(np.swapaxes(sqz, 0, 2), 0, 1)
     
     # Swap BGR to RGB (this appears unnecessary in general)
     # rgb = cv2.cvtColor(tmp, cv2.COLOR_BGR2RGB)
@@ -73,9 +73,11 @@ def warp(img, flow):
         Tensor: warped image or feature map
     '''
     height, width = flow.shape[:2]
-    flow[:, :, 0] += np.arange(width)
-    flow[:, :, 1] += np.arange(height)[:, np.newaxis]
-    out = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
+    # Don't do in-place modifications!
+    flow_copy = np.copy(flow)
+    flow_copy[:, :, 0] += np.arange(width)
+    flow_copy[:, :, 1] += np.arange(height)[:, np.newaxis]
+    out = cv2.remap(img, flow_copy, None, cv2.INTER_LINEAR)
     return out
 
 def count_files(dir, extension):